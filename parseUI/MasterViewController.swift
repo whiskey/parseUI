fd6df3bb0aed5c029180525f291951beb79e7666
Carsten Witzke <carsten@staticline.de>
2014-10-17 16:39:08 +0100

    implemented naïve push control to all platforms

--- parseUI/MasterViewController.swift
+++ parseUI/MasterViewController.swift
@@ -7,12 +7,11 @@
 //
 
 import UIKit
+import Parse
 
 class MasterViewController: UITableViewController {
-
-    var detailViewController: DetailViewController? = nil
-    var objects = NSMutableArray()
-
+    
+    var detailViewController: PushViewController? = nil
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -25,25 +24,24 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
         if let split = self.splitViewController {
             let controllers = split.viewControllers
-            self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
+            self.detailViewController = controllers[controllers.count-1].topViewController as? PushViewController
         }
+        
+        setupParse()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func insertNewObject(sender: AnyObject) {
-        objects.insertObject(NSDate.date(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+    
+    // MARK: - Parse setup
+    
+    func setupParse() {
+        // TODO: get local config
+        Parse.setApplicationId("", clientKey: "")
     }
 
     // MARK: - Segues
@@ -51,9 +49,18 @@ class MasterViewController: UITableViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow() {
-                let object = objects[indexPath.row] as NSDate
-                let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
-                controller.detailItem = object
+                switch indexPath.row {
+                case 0:
+                    println("case 0")
+                    break
+                case 1:
+                    println("case 1")
+                    break
+                default:
+                    println("default")
+                }
+                
+                let controller = (segue.destinationViewController as UINavigationController).topViewController as PushViewController
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
@@ -62,36 +69,21 @@ class MasterViewController: UITableViewController {
 
     // MARK: - Table View
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
-    }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
-
-        let object = objects[indexPath.row] as NSDate
-        cell.textLabel?.text = object.description
-        return cell
-    }
-
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            objects.removeObjectAtIndex(indexPath.row)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
-    }
-
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        return 1
+//    }
+//
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return objects.count
+//    }
+//
+//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+//
+//        let object = objects[indexPath.row] as NSString
+//        cell.textLabel?.text = object
+//        return cell
+//    }
 
 }
 
---
+++ parseUI/PushViewController.swift
@@ -0,0 +1,85 @@
+//
+//  PushViewController.swift
+//  parseUI
+//
+//  Created by Carsten Witzke on 17/10/14.
+//  Copyright (c) 2014 Secretescapes. All rights reserved.
+//
+
+import UIKit
+
+class PushViewController: UIViewController {
+
+    @IBOutlet weak var alertTextView: UITextView!
+    @IBOutlet weak var defaultSoundSwitch: UISwitch!
+    @IBOutlet weak var backgroundPushSwitch: UISwitch!
+    @IBOutlet weak var customPayloadTextView: UITextView!
+    @IBOutlet weak var targetControl: UISegmentedControl!
+    @IBOutlet weak var sendNotificationButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        alertTextView.text = "test message \(NSDate())"
+        customPayloadTextView.text = ""
+        targetControl.selectedSegmentIndex = 0
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onChannelChange(sender: AnyObject) {
+        println("selected \(targetControl.selectedSegmentIndex)")
+    }
+    
+    // MARK: - push handling
+    
+    @IBAction func onSendPush(sender: AnyObject) {
+        sendPush()
+    }
+    
+    func sendPush() {
+        // prepare standard push payload
+        var push = PFPush()
+        var data: [String: String] = Dictionary()
+        if (alertTextView.text != nil) {
+            data["alert"] = alertTextView.text
+        }
+        if (defaultSoundSwitch.enabled) {
+            data["sound"] = "default"
+        }
+        if (backgroundPushSwitch.enabled) {
+            data["content-available"] = "1"
+        }
+        print(data)
+        assert(data.count>0, "no data given!")
+        
+        // custom payload data
+        
+        push.setData(data)
+        
+        // channels
+        var channels = []
+        switch targetControl.selectedSegmentIndex {
+        case 1:
+            channels = ["beta_testers", "devs"]
+        default:
+            channels = ["devs"]
+        }
+        push.setChannels(channels)
+        
+        // go!
+        var error = NSErrorPointer()
+        push.sendPushInBackgroundWithBlock { (success, error) -> Void in
+            if error != nil {
+                print(error)
+            }
+            if success {
+                println("sending push notifications done: \(channels)")
+            }
+        }
+    }
+
+}
